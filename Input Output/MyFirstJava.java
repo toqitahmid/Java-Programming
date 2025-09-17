580e1a616605df90c6ee9633500bc2353b467327
Toqi Tahmid <rubaiathali@gmail.com>
2025-09-17 10:38:48 +0600

    Updated MyfirstJava

--- Input Output/MyFirstJava.java
+++ Input Output/MyFirstJava.java
@@ -3,5 +3,6 @@ public class MyFirstJava {
     public static void main(String args[]){
 
         System.out.println("Hello Github!");
+        System.out.println("This repo is all about Java-Programming");
     }
 }
