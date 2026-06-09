d01fc867657e8e61948f84010c65231e5b901da3
toqitahmid <rubaiathali@gmail.com>
2026-06-09 22:39:52 +0600

    learn abstractor

---
+++ Absractor/Araf.java
@@ -0,0 +1,7 @@
+public class Araf extends LearnAbstractor {
+
+    @Override
+    public void Display() {
+        System.out.println("Hi, this is Araf");
+    }
+}
---
+++ Absractor/MainAbstractor.java
@@ -0,0 +1,7 @@
+public class MainAbstractor{
+    public static void main(String[] args) {
+        LearnAbstractor ab;
+        ab = new Araf();
+        ab.Display();
+    }
+}
